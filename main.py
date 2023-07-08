017a6cb2192dc5cd88ab8ce114015fd843853916
Artyom Guryanov <bishop81285@gmail.com>
2023-07-08 20:12:04 +0600

    bot_v1.0 completed

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 from dotenv import load_dotenv
+from telegram import Bot
+from telegram.utils.request import Request
 
 CURRENCY_RATES_FILE = "currency_rates.json"
 load_dotenv()
 API_KEY = os.environ['EXCHANGE_RATES_API_KEY']
+BOT_TOKEN = os.environ['TG_BOT_API']
+CHAT_ID = [os.environ['TG_CHAT_ID_1'], os.environ['TG_CHAT_ID_2']]
+CHECK_INTERVAL = timedelta(minutes=30)
+
+bot = Bot(token=BOT_TOKEN, request=Request())
 
 
 def get_currency_rate(currency: str) -> float:
@@ -35,13 +42,18 @@ def save_to_json(data: dict) -> None:
                 json.dump(data_list, json_file)
 
 
-def main():
+def send_update_to_telegram(data: dict) -> None:
+    """Отправляет обновление о курсе валюты в Telegram"""
+    message = f"Курс {data['currency']} к рублю: {data['rate']:.2f}"
+    for chat_id in CHAT_ID:
+        bot.send_message(chat_id=chat_id, text=message)
+
+
+def check_currency_rate() -> None:
     """
-    Основная функция программы.
-    Получает от пользователя название валюты — USD или EUR,
-    получает и выводит на экран текущий курс валюты от API.
-    Записывает данные в JSON-файл.
-    """
+        Проверяет курс валюты и отправляет обновления в Telegram,
+        если курс отличается от предыдущего сохраненного значения
+        """
     while True:
         currency = input("Введите название валюты (USD или EUR): ").upper()
 
@@ -54,7 +66,18 @@ def main():
 
         print(f"Курс {currency} к рублю: {rate:.2f}")
         data = {"currency": currency, "rate": rate, "timestamp": timestamp}
-        save_to_json(data)
+
+        previous_data = None
+
+        with open(CURRENCY_RATES_FILE) as json_file:
+            data_list = json.load(json_file)
+
+            if data_list:
+                previous_data = data_list[-1]
+
+        if previous_data is None or data["rate"] != previous_data["rate"]:
+            save_to_json(data)
+            send_update_to_telegram(data)
 
         choice = input("Выберите действие: (1 - продолжить, 2 - выйти) ")
 
@@ -64,6 +87,20 @@ def main():
             break
         else:
             print("Некорректный ввод")
+
+
+def main():
+    """
+    Основная функция программы.
+    Проверяет курс валюты и отправляет обновления в Telegram,
+    если курс отличается от предыдущего сохраненного значения.
+    """
+    while True:
+        check_currency_rate()
+        next_check = datetime.now() + CHECK_INTERVAL
+        print(f"Следующая проверка будет выполнена в {next_check.strftime('%Y-%m-%d %H:%M:%S')}")
+        while datetime.now() < next_check:
+            pass
 
 
 if __name__ == "__main__":
